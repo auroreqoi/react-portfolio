7aeb0b16ebdacb4255737807c86c981142410d4e
aurore rigault <rigault.aurore@gmail.com>
2022-11-05 21:38:01 +0100

    porftolio page done

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faTwitch } from '@fortawesome/free-brands-svg-icons'
 
 
@@ -17,10 +17,13 @@ const Sidebar = () => (
       <NavLink exact='true' activeclassname='active' to='/'>
         <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
       </NavLink>
-      <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
+      <NavLink activeclassname='active' className='about-link' to='/about'>
         <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
       </NavLink>
-      <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
+      <NavLink activeclassname="active" className="portfolio-link" to='/portfolio'>
+        <FontAwesomeIcon icon={faSuitcase} color='#4d4d4e'/>
+      </NavLink>
+      <NavLink activeclassname='active' className='contact-link' to='/contact'>
         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
       </NavLink>
     </nav>
