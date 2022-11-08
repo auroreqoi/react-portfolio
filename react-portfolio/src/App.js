54d141f5141d153254226ade50aa1188e08be538
aurore rigault <rigault.aurore@gmail.com>
2022-11-08 16:11:04 +0100

    heroku suppr dashboard & login

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -5,7 +5,6 @@ import Home from './components/Home'
 import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
-import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Route path="about" element={<About />}/>
         <Route path="/contact" element={<Contact />}/>
         <Route path="/portfolio" element={<Portfolio />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
       </Route>
     </Routes>
     </>
